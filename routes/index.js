e12f672bb9481e9d923d2eee7a960583859217be
amaranto <amaranto_yo@hotmail.com>
2015-06-08 02:05:05 +0200

    Borrar pregunta

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,8 @@ router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
 
 router.put('/quizes/:quizId(\\d+)', quizController.update);
 
+router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+
 router.get("/author",quizController.author);
 
 module.exports = router;
